1d56ae5970750770000a06f4b619fa1279fcc667
navraj kambo <navrajkambo@gmail.com>
2018-08-04 15:29:18 -0700

    - android
    - added android native code to project
    - update license
    - updated package.json

---
+++ android/src/main/java/com/ndart/RNSaveAudio/RNSaveAudioModule.java
@@ -0,0 +1,116 @@
+package com.ndart.RNSaveAudio;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableType;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.DataOutputStream;
+import java.nio.ByteBuffer;
+import java.io.IOException;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RNSaveAudioModule extends ReactContextBaseJavaModule {
+    public RNSaveAudioModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+    
+    @Override
+    public String getName() {
+        return "RNSaveAudio";
+    }
+    
+    @ReactMethod
+    public void saveWav(string path, ReadableArray audio, final Promise promise);
+        try {
+            byte[] newaudio;
+            for(int i=0; i<audio.size();i++){
+                newaudio.add((byte) audio[i]);
+            }
+            boolean result = SaveFile(path, newaudio);
+            promise.resolve(result);
+        } catch (Exception ex) {
+            promise.reject("ERR_UNEXPECTED_EXCEPTION", ex);
+        }
+    }
+    private boolean SaveFile(string path, byte[] rawData){
+
+        DataOutputStream output = null;
+        try {
+            output = new DataOutputStream(new FileOutputStream(path));
+            // WAVE header
+            // see http://ccrma.stanford.edu/courses/422/projects/WaveFormat/
+            writeString(output, "RIFF"); // chunk id
+            writeInt(output, 36 + rawData.length); // chunk size
+            writeString(output, "WAVE"); // format
+            writeString(output, "fmt "); // subchunk 1 id
+            writeInt(output, 16); // subchunk 1 size
+            writeShort(output, (short) 1); // audio format (1 = PCM)
+            writeShort(output, (short) 1); // number of channels
+            writeInt(output, 44100); // sample rate
+            writeInt(output, 44100 * 2); // byte rate
+            writeShort(output, (short) 2); // block align
+            writeShort(output, (short) 16); // bits per sample
+            writeString(output, "data"); // subchunk 2 id
+            writeInt(output, rawData.length); // subchunk 2 size
+            // Audio data (conversion big endian -> little endian)
+            short[] shorts = new short[rawData.length / 2];
+            ByteBuffer.wrap(rawData).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(shorts);
+            ByteBuffer bytes = ByteBuffer.allocate(shorts.length * 2);
+            for (short s : shorts) {
+                bytes.putShort(s);
+            }
+            for()
+            output.write(rawData);
+        } finally {
+            if (output != null) {
+                output.close();
+                return true
+            }
+        } catch {
+return false;
+        }
+    }
+
+    private byte[] short2byte(short[] sData) {
+        int shortArrsize = sData.length;
+        byte[] bytes = new byte[shortArrsize * 2];
+        for (int i = 0; i < shortArrsize; i++) {
+            bytes[i * 2] = (byte) (sData[i] & 0x00FF);
+            bytes[(i * 2) + 1] = (byte) (sData[i] >> 8);
+            sData[i] = 0;
+        }
+        return bytes;
+    }
+
+    private void writeInt(final DataOutputStream output, final int value) throws IOException {
+        output.write(value >> 0);
+        output.write(value >> 8);
+        output.write(value >> 16);
+        output.write(value >> 24);
+    }
+
+    private void writeShort(final DataOutputStream output, final short value) throws IOException {
+        output.write(value >> 0);
+        output.write(value >> 8);
+    }
+
+    private void writeString(final DataOutputStream output, final String value) throws IOException {
+        for (int i = 0; i < value.length(); i++) {
+            output.write(value.charAt(i));
+        }
+    }
+}
